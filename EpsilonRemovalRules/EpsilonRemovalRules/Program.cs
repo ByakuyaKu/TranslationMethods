18fa7da8ddf73d9625b46badc5d9096f98686599
Toga <rodionphilippov@gmail.com>
2019-11-13 01:35:00 +0400

    not final

---
+++ EpsilonRemovalRules/EpsilonRemovalRules/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+
+namespace EpsilonRemovalRules
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<Char> Q = new Queue<char>();
+
+            List<Rules> Grammar = new List<Rules>();
+
+            Grammar = Rules.ReadRulesInTxt();
+
+            Console.WriteLine("Input grammar:");
+            foreach (var item in Grammar)
+                Console.WriteLine(item.leftSide + " -> " + item.rightSide);
+
+            for (int i = 0; i < Grammar.Count; i++)
+            {
+                if (Rules.CheckEps(Grammar[i].rightSide))
+                {
+                    //Grammar[i].isEpsilon = true;
+                    Q.Enqueue(Grammar[i].leftSide);
+                    Grammar.RemoveAt(i);
+                }
+                else if (Rules.CheckLowerChars(Grammar[i].rightSide))
+                    Grammar.RemoveAt(i);
+            }
+
+            while (Q.Count != 0)
+            {
+                var element = Q.Dequeue();
+
+                for (int i = 0; i < Grammar.Count; i++)
+                {
+                    for (int j = 0; j < Grammar[i].rightSide.Length; j++)
+                    {
+                        if (element == Grammar[i].rightSide[j])
+                            Grammar[i].counter--;
+                    }
+
+                    if (Grammar[i].counter == 0)
+                    {
+                        Q.Enqueue(Grammar[i].leftSide);
+                       // Grammar[i].isEpsilon = true;
+                        Grammar.RemoveAt(i);
+                    }
+                }
+            }
+
+            Console.WriteLine("Result");
+            foreach (var item in Grammar)
+                //if (!item.isEpsilon)
+                    Console.WriteLine(item.leftSide + " -> " + item.rightSide);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EpsilonRemovalRules/EpsilonRemovalRules/Rules.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace EpsilonRemovalRules
+{
+    public class Rules
+    {
+        public Char leftSide;
+        public string rightSide;
+        public bool isEpsilon;
+        public int counter;
+        public static int counterLowerChars;
+
+        public Rules(Char c, string s)
+        {
+            leftSide = c;
+            rightSide = s;
+            counter = s.Length;
+            isEpsilon = false;
+        }
+        
+        public static bool CheckLowerChars(string s)
+        {
+            for (int i = 0; i < s.Length; i++) 
+                if(!Char.IsUpper(s[i]))
+                    counterLowerChars++;
+
+            if (counterLowerChars == s.Length)
+                return true;
+
+            return false;
+        }
+
+        public static bool CheckEps(string s)
+        {
+            for (int i = 0; i < s.Length; i++)
+                if (s[i] == 'E')
+                    return true;
+
+            return false;
+        }
+
+        public static List<Rules> ReadRulesInTxt()
+        {
+            string path = @"D:\TranslationMethods\EpsilonRemovalRules\text3.txt";
+            var CurLine = "";
+            List<Rules> Grammar = new List<Rules>();
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        CurLine = sr.ReadLine();
+                        Grammar.Add(new Rules(CurLine[0], CurLine.Substring(1, CurLine.Length -1)));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return Grammar;
+        }
+    }
+}
